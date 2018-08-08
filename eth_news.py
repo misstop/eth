3b08b30a7bdc9767276e9215e15617176be6bff0
longfei <lilongfei@wallstreetcn.com>
2018-08-08 17:15:42 +0800

    update

--- eth_news.py
+++ eth_news.py
@@ -21,8 +21,65 @@ logging.basicConfig(level=logging.DEBUG,
                     filemode='a')
 
 
+# 解析yaml
+cur_path = os.path.dirname(os.path.realpath(__file__))
+x = yaml.load(open('%s/config.yml' % cur_path, encoding='UTF-8'))
+# 数据库
+host = x['DATADBO']['MYSQL']['HOST']
+username = x['DATADBO']['MYSQL']['UNAME']
+pwd = x['DATADBO']['MYSQL']['PWD']
+database = x['DATADBO']['MYSQL']['DNAME']
+
+
+# 数据库连接
+def connect_db():
+    logging.info('start to connect mysql')
+    db = pymysql.connect('{}'.format(host), '{}'.format(username), '{}'.format(pwd), '{}'.format(database))
+    logging.info('connect success')
+    return db
+
+
+# 插入语句
+def insert_db(db, txHash, txReceipt, block, timeStamp, From, to, value, limit, used, price, actual, nonce, inputData):
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+    # SQL 插入语句
+    sql = "INSERT INTO eth_news VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on duplicate key update txHash = values(txHash)"
+    par = (txHash, txReceipt, block, timeStamp, From, to, value, limit, used, price, actual, nonce, inputData)
+    try:
+        # 执行sql语句
+        cursor.execute(sql, par)
+        # 提交到数据库执行
+        db.commit()
+        logging.info("insert success")
+    except Exception as e:
+        logging.error(e)
+        db.rollback()
+
+
+# 查询语句
+def query_db(db, txHash):
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+    sql = "SELECT * FROM Student WHERE txHash = %s" % txHash
+    try:
+        # 执行SQL语句
+        cursor.execute(sql)
+        # 获取所有记录列表
+        results = cursor.fetchall()
+        for row in results:
+            return row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12]
+    except Exception as e:
+        logging.error(e)
+
+
+# 关闭数据库
+def close_db(db):
+    db.close()
+
+
 # 提供抓取接口
-@app.route('/address/<site>/', methods=['GET', 'POST'])
+@app.route('/address/<site>/', methods=["POST"])
 def get_msg(site):
     baseurl = 'https://etherscan.io/tx/'
     try:
@@ -39,37 +96,68 @@ def get_msg(site):
     timeStamp = strTime.group(1)
     From = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][5]/a")[0].text
     to = html.xpath("//a[contains(@class, 'wordwrap')]")[0].text
-    value = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][7]/span")[0].text
-    limit = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][8]/span")[0].text
-    used = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][9]/span")[0].text
+    value = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][7]/span")[0].text.replace("\n", "")
+    limit = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][8]/span")[0].text.replace("\n", "")
+    used = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][9]/span")[0].text.replace("\n", "")
     price_ls = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][10]/span")
-    price = price_ls[0].xpath('string(.)')
+    price = price_ls[0].xpath('string(.)').replace("\n", "")
     actual_ls = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][11]/span")
-    actual = actual_ls[0].xpath('string(.)')
+    actual = actual_ls[0].xpath('string(.)').replace("\n", "")
     nonce = html.xpath("//div[contains(@class, 'col-sm-9 cbs')][12]/span")[0].text
     inputData = html.xpath("//textarea")[0].text
-    # private = html.xpath("//div/font")[0].text
+    try:
+        db = connect_db()
+        insert_db(db, txHash, txReceipt, int(block), timeStamp, From, to, value, limit, used, price, actual, nonce, inputData)
+    except Exception as e:
+        close_db(db)
+        return e
+    close_db(db)
+    return "success"
+
+
+# 从数据库查询
+@app.route('/address/<site>/', methods=["GET"])
+def select_msg(site):
+    # 使用cursor()方法获取操作游标
+    db = connect_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM eth_news WHERE txHash = '%s'" % site
+    try:
+        # 执行SQL语句
+        cursor.execute(sql)
+        # 获取所有记录列表
+        row = cursor.fetchone()
+        # print(row)
+    except Exception as e:
+        logging.error(e)
+        return "failed"
     dic = {
-        "txHash": txHash,
-        "txReceipt": txReceipt,
-        "block": block,
-        "timeStamp": timeStamp,
-        "From": From,
-        "to": to,
-        "value": value.replace("\n", ""),
-        "limit": limit.replace("\n", ""),
-        "used": used.replace("\n", ""),
-        "price": price.replace("\n", ""),
-        "actual": actual.replace("\n", ""),
-        "nonce": nonce,
-        "inputData": inputData,
-        "private": "<To access the private Note feature, you must be logged in>",
+        "txHash": row[0],
+        "txReceipt": row[1],
+        "block": row[2],
+        "timeStamp": row[3],
+        "From": row[4],
+        "to": row[5],
+        "value": row[6],
+        "limit": row[7],
+        "used": row[8],
+        "price": row[9],
+        "actual": row[10],
+        "nonce": row[11],
+        "inputData": row[12],
+        # "private": "<To access the private Note feature, you must be logged in>",
     }
-    return str(dic)
+    if dic:
+        close_db(db)
+        return str(dic)
+    else:
+        close_db(db)
+        return 'dic is null'
 
 
 if __name__ == '__main__':
     # get_msg('0xf4a42c5afca3fc44a119c02d399364d07292bf67d369817496372f08709f6df0')
     app.run(
         host='0.0.0.0',
-        port=5000, debug=False)
+        port=5000, debug=True)
+
